c0b3e3f0b5042c28422320d469d74ade25002d91
Jeeveth-K-27 <jeevethkrishnan@gmail.com>
2026-02-10 18:45:19 +0530

    EOD 10.02.2025

--- Frontend/src/Components/Products/NewArrivals.jsx
+++ Frontend/src/Components/Products/NewArrivals.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { IoIosArrowDropleft, IoIosArrowDropright  } from "react-icons/io";
 import { Link } from 'react-router-dom';
 
@@ -8,7 +8,42 @@ const NewArrivals = () => {
     const [isDragging, setIsDragging] = useState(false);
     const [startX, setStartX] = useState(0);
     const [scrollLeft, setScrollleft] = useState(false);
-    const [scrollRight, setScrollRight] = useState(false);
+    const [CanScrollLeft, setCanScrollLeft] = useState(false);
+    const [CanScrollRight, setCanScrollRight] = useState(true);
+
+    const scroll = (dir) => {
+        console.log("press")
+        const scrollAmount = dir === "left" ? -300 : 300;
+        scrollRef.current.scrollBy({left: scrollAmount, behavior: "smooth"});
+    }
+
+    const updateScrollButtons = () => {
+       const container = scrollRef.current;
+
+        if(container){
+            const leftScroll = container.scrollLeft;
+            const righttScroll = container.scrollWidth > container.scrollLeft + container.clientWidth;
+            
+            
+            setCanScrollLeft(leftScroll > 0);
+            setCanScrollRight(righttScroll)
+        }
+
+       console.log({
+        scrollLeft : container.scrollLeft,
+        clientWidth : container.clientWidth,
+        containerScrollWidth : container.scrollWidth,
+       }) 
+    }
+
+    useEffect(() => {
+        const container = scrollRef.current;
+        if(container){
+            container.addEventListener("scroll", updateScrollButtons);
+            updateScrollButtons();
+            return () => container.removeEventListener("scroll", updateScrollButtons);
+        }
+    }, []);
 
     const newArrivals = [
         {
@@ -110,12 +145,12 @@ const NewArrivals = () => {
             </p>
 
             {/* Scroll Button */}
-             <div className='absolute right-[40%] -bottom-8 md:right-0 md:bottom-[-30px] flex space-x-2'>
-                <button className='p-2  border rounded-full bg-white text-black'>
+             <div className='absolute -right-6 -bottom-8 md:right-0 md:bottom-[-30px] flex space-x-2'>
+                <button onClick={() => scroll("left")} disabled={!CanScrollLeft} className={`p-2  border rounded-full ${CanScrollLeft ? " bg-white text-black" : " bg-gray-200 text-gray-400"}`}>
                     <IoIosArrowDropleft className='text-base md:text-2xl'/>
                 </button>
 
-                <button className='p-2  border rounded-full bg-white text-black'>
+                <button onClick={() => scroll("right")} disabled={!CanScrollRight} className={`p-2  border rounded-full ${CanScrollRight ? " bg-white text-black" : " bg-gray-200 text-gray-400"}`}>
                     <IoIosArrowDropright className='text-base md:text-2xl'/>
                 </button>
             </div>  
@@ -123,12 +158,12 @@ const NewArrivals = () => {
         </div>
 
         {/* Scrollable Content */}
-        <div className='flex mx-10 overflow-x-auto space-x-6 relative scrollbar-hide mb-8'>
+        <div ref={scrollRef} className='flex mx-4 md:mx-10 overflow-x-auto space-x-6 relative scrollbar-hide mb-8'>
 
             {
                 newArrivals.map((product) => (
                     <div key={product.id} className='min-w-[100%] sm:min-w-[50%] lg:min-w-[30%] relative'>
-                        <img src={product.images[0]?.url} alt={product.images[0]?.altText || product.name} className='w-full h-[500px] object-cover rounded-lg'/>
+                        <img src={product.images[0]?.url} alt={product.images[0]?.altText || product.name} draggable={false} className='w-full h-[500px] object-cover rounded-lg'/>
 
                         <div className='absolute bottom-0 left-0 right-0 bg-opacity-50 backdrop-blur-md text-white p-4 rounded-b-lg'>
                             <Link to={`/product/${product.id}`} className='block'><h4 className='font-medium'>{product.name}</h4></Link>
